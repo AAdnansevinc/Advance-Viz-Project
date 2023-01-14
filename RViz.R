c375b1d201362c30719277c704349e1ec5722957
Pacholleck <60796009+Pacholleck@users.noreply.github.com>
2023-01-14 14:23:29 +0100

    Add world map

--- RViz.R
+++ RViz.R
@@ -7,7 +7,10 @@ pacman::p_load(
                "ggplot2",
                "tidyverse",
                "dplyr",
-               "CGPfunctions"
+               "CGPfunctions",
+               "rnaturalearth",
+               "sf",
+               "cowplot"
 )
 
 
@@ -25,7 +28,12 @@ nrow(data)
 data <- data[complete.cases(data),]  #only complete dataset
 nrow(data)
 
-
+# Add missing years
+countries <- unique(data$Country)
+years <- unique(data$Year)
+all <- data.frame(expand.grid(countries,years))
+names(all) <- c("Country", "Year")
+data <- left_join(all,data, by = c("Country"="Country", "Year" = "Year"))
 
 #drop the below columns
 # ["NegativeEffect","PositiveEffect","Confidence"]
@@ -66,7 +74,66 @@ summary(data)
 #1) Map #Dustin_3
 
 # 1.1) Countries
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+
+data_2021 <- data %>% 
+  mutate(HappinessScore = round(HappinessScore,2)) %>% 
+  filter(Year == 2021)
+
+data_world <- full_join(data_2021, world, by = c("Country"="name"))
+
+theme_set(theme_bw())
+
+gworld_base <- 
+  ggplot(data = data_world) +
+  # geom_sf(aes(fill = income_grp)) +
+  coord_sf(expand = FALSE) +
+  geom_sf(aes(geometry =  geometry, fill =  HappinessScore)) +
+  scale_fill_gradient(low="red", high="green",na.value = "grey") 
+  
+gworld <- 
+  gworld_base +
+  theme(legend.position = 'bottom',
+        plot.title = element_text(size = 20)) +
+  labs(title = "Happieness Worldwide in 2021")
+
+gworld
+
 # 1.2) Europe
+
+gworld_sub <- 
+  gworld_base +
+  geom_rect(xmin = -30, xmax = 50, ymin = 35, ymax = 70, 
+            fill = NA, colour = "black", size = 1.5) +
+  theme(legend.position= 'none',
+        axis.text = element_blank(),
+        axis.ticks=element_blank(),
+        )
+
+
+geurope <- ggplot(data = data_world) +
+  # geom_sf(aes(fill = income_grp)) +
+  coord_sf(expand = FALSE) +
+  geom_sf(aes(geometry =  geometry, fill =  HappinessScore)) +
+  scale_fill_gradient(low="red", high="green",na.value = "grey") +
+  coord_sf(xlim = c(-30, 50), ylim = c(35, 70), expand = TRUE) +
+  # scale_fill_brewer(palette = 'YlOrBr') +
+  # scale_fill_viridis_c(option = "plasma", trans = "sqrt", 
+  #                      breaks = seq(0, 10e09, by=250e6),
+  #                      labels = paste(seq(0, 10e09, by=250e6)/1e6, 'mln'),
+  #                      guide = guide_colorbar(barwidth = 30), name = NULL) +
+  # theme(legend.position = 'right') 
+  theme(legend.position = 'bottom',
+        plot.title = element_text(size = 20)) +
+  labs(title = "Happieness Europe in 2021")
+
+ggdraw() +
+  draw_plot(geurope,
+            0, 0, 1, 1) +
+  draw_plot(gworld_sub, 
+            0.03, 0.25, 0.2, 0.2)
+
 
 #2) Slope Graph in ggplot2 or  most probably Bump Chart # Adnan_1
 # European countries, bottom 10.
